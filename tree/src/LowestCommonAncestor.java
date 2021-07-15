517afea831dcd08c9f74b50820482f644ea1a7e0
Witold Zolnowski <blue3656@gmail.com>
2021-07-15 21:51:18 +0100

    working solution

--- tree/src/LowestCommonAncestor.java
+++ tree/src/LowestCommonAncestor.java
@@ -1,7 +1,6 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
-class Node {
+class LowestCommonAncestor {
   Node left;
   Node right;
   int data;
@@ -15,35 +14,17 @@ class Node {
 
 class Solution {
 
-  /*
-  class Node
-      int data;
-      Node left;
-      Node right;
-  */
   public static Node lca(Node root, int v1, int v2) {
-    ArrayList<Node> p1 = new ArrayList<>();
-    ArrayList<Node> p2 = new ArrayList<>();
-    ArrayList<Node> pv1 = traverse(root, v1, p1);
-    ArrayList<Node> pv2 = traverse(root, v2, p2);
-    int maxLen = pv1.size() <= pv2.size() ? pv1.size() : pv2.size();
-
-    for (int i = maxLen - 1; i >= 0; i--) {
-      for (int j = maxLen - 1; j >= 0; j--) {
-        if (pv1.get(i) == pv2.get(j)) return pv1.get(i);
-      }
+    //if both values are bigger than current root node, we can move 'right'
+    if (root.data < v1 && root.data < v2) {
+      return lca(root.right, v1, v2);
     }
-    return null;
-  }
-
-  static ArrayList<Node> traverse(Node curr, int target, ArrayList<Node> path) {
-    if (curr == null) return null;
-    path.add(curr);
-    if (curr.data == target) return path;
-    ArrayList<Node> left = traverse(curr.left, target, path);
-    ArrayList<Node> right = traverse(curr.right, target, path);
-    if (left != null) return left;
-    return right;
+    //if both values are smaller than current root node, we can move 'left'
+    if (root.data > v1 && root.data > v2) {
+      return lca(root.left, v1, v2);
+    }
+    //if one value is bigger & other  smaller than root -> we have a common ancestor
+    return root;
   }
 
   public static Node insert(Node root, int data) {
