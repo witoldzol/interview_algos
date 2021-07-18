f6cfa5caeb374eb5181e80be49b59dec7eb2484f
Witold Zolnowski <blue3656@gmail.com>
2021-07-18 15:43:41 +0100

    cleaned up solution

--- tree/src/TreeValidator.java
+++ tree/src/TreeValidator.java
@@ -7,12 +7,7 @@ public class TreeValidator {
 
   private boolean validate(Node root, int min, int max) {
     if (root == null) return true;
-    if (root.left != null) {
-      if (root.left.data > root.data || root.left.data <= min) return false;
-    }
-    if (root.right != null) {
-      if (root.right.data < root.data || root.right.data >= max) return false;
-    }
+    if (root.data <= min || root.data >= max) return false;
     return validate(root.left, min, root.data) && validate(root.right, root.data, max);
   }
 }
