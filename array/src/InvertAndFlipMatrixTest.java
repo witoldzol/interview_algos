c7841f82b848813bec608a93716c68d70048412e
Witold Zolnowski <blue3656@gmail.com>
2021-07-28 21:41:41 +0100

    new project for array questions

---
+++ array/src/InvertAndFlipMatrixTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InvertAndFlipMatrixTest {
+
+  @Test
+  public void returnsTrue(){
+    assertTrue(true);
+  }
+
+}
