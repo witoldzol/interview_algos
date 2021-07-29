efdff3c0f2bcd0e21bfa81b9842a83429ee94746
Witold Zolnowski <blue3656@gmail.com>
2021-07-29 20:29:44 +0100

    three by three - failing test

--- array/src/InvertAndFlipMatrix.java
+++ array/src/InvertAndFlipMatrix.java
@@ -1,2 +1,15 @@
 public class InvertAndFlipMatrix {
+  public int[][] flipAndInvert(int[][] matrix) {
+    return invert(matrix);
+  }
+
+  private int[][] invert(int[][] matrix) {
+    int middle = matrix.length/2;
+    for(int i=0;i<matrix.length;i++){
+      for (int j = 0; j<middle; j++) {
+        matrix[i][j] = (matrix[i][j] == 0) ? 1 : 0;
+      }
+    }
+    return matrix;
+  }
 }
--- array/src/InvertAndFlipMatrixTest.java
+++ array/src/InvertAndFlipMatrixTest.java
@@ -1,12 +1,42 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Arrays;
 
 class InvertAndFlipMatrixTest {
+  InvertAndFlipMatrix underTest;
 
-  @Test
-  public void returnsTrue(){
-    assertTrue(true);
+  @BeforeEach
+  void setup() {
+    underTest = new InvertAndFlipMatrix();
   }
 
+  @Test
+  public void zeroArray() {
+    int[][] matrix = {{0}};
+    int[][] expected = {{1}};
+    Assertions.assertArrayEquals(expected, underTest.flipAndInvert(matrix));
+  }
+
+  @Test
+  public void twoElementArray() {
+    int[][] matrix = {{1}, {0}};
+    int[][] expected = {{0}, {1}};
+    Assertions.assertArrayEquals(expected, underTest.flipAndInvert(matrix));
+  }
+
+  @Test
+  public void twoByTwo() {
+    int[][] matrix = {{1, 1}, {0, 0}};
+    int[][] expected = {{0, 0}, {1, 1}};
+    Assertions.assertArrayEquals(expected, underTest.flipAndInvert(matrix));
+  }
+
+  @Test
+  public void threeByThree() {
+    int[][] matrix = {{1, 1, 0}, {1, 0, 0}};
+    int[][] expected = {{1, 0, 0}, {1, 1, 0}};
+    System.out.println(Arrays.deepToString(underTest.flipAndInvert(matrix)));
+    Assertions.assertArrayEquals(expected, underTest.flipAndInvert(matrix));
+  }
 }
