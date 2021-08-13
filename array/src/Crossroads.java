5d69a689872953d27ff1d73c2f6af3dfab00e060
Witold Zolnowski <blue3656@gmail.com>
2021-08-13 21:48:28 +0100

    finished refactor

--- array/src/Crossroads.java
+++ array/src/Crossroads.java
@@ -4,15 +4,15 @@ public class Crossroads {
   }
 
   int visit(int i, int j, Matrix m, int crossroads) {
-    if (isCoordinatesValid(i, j, m)) {
-      if (isEnd(m.getArr()[i][j])) {
+    if (m.isCoordinatesValid(i, j)) {
+      if (m.isEnd(i, j)) {
         return crossroads;
       }
-      if (!isValidPath(m.getArr()[i][j])) {
+      if (!m.isValidPath(i, j)) {
         return crossroads;
       }
-      markCellAsVisited(i, j, m);
-      int p = calculateCrossroads(i, j, m);
+      m.markCellAsVisited(i, j);
+      int p = m.calculateCrossroads(i, j);
       if (p == 0) {
         return 0;
       }
@@ -29,14 +29,6 @@ public class Crossroads {
     }
   }
 
-  boolean isEnd(char c) {
-    return c == 'C';
-  }
-
-  boolean isValidPath(char c) {
-    return (c == 'P' || c == 'S');
-  }
-
   int goLeft(int i, int j, Matrix m, int crossroads) {
     return visit(i, j - 1, m, crossroads);
   }
@@ -51,52 +43,5 @@ public class Crossroads {
 
   int goDown(int i, int j, Matrix m, int crossroads) {
     return visit(i - 1, j, m, crossroads);
-  }
-
-  private void markCellAsVisited(int i, int j, Matrix m) {
-    m.getArr()[i][j] = 'V';
-  }
-
-  private boolean isCoordinatesValid(int i, int j, Matrix m) {
-    int maxI = m.getArr().length;
-    int maxJ = 0;
-    if (i >= 0 && i < maxI) {
-      maxJ = m.getArr()[i].length;
-    }
-    return i >= 0 && i < maxI && j >= 0 && j < maxJ;
-  }
-
-  int calculateCrossroads(int i, int j, Matrix m) {
-    int maxI = m.getArr().length;
-    int maxJ = 0;
-    if (i >= 0 && i < maxI) {
-      maxJ = m.getArr()[i].length;
-    }
-    int paths = 0;
-    //above
-    if (i - 1 >= 0) {
-      if (m.getArr()[i - 1][j] == 'C' || m.getArr()[i - 1][j] == 'P') {
-        paths += 1;
-      }
-    }
-    //below
-    if (i + 1 < maxI) {
-      if (m.getArr()[i + 1][j] == 'C' || m.getArr()[i + 1][j] == 'P') {
-        paths += 1;
-      }
-    }
-    //left
-    if (j - 1 >= 0) {
-      if (m.getArr()[i][j - 1] == 'C' || m.getArr()[i][j - 1] == 'P') {
-        paths += 1;
-      }
-    }
-    //left
-    if (j + 1 < maxJ) {
-      if (m.getArr()[i][j + 1] == 'C' || m.getArr()[i][j + 1] == 'P') {
-        paths += 1;
-      }
-    }
-    return paths;
   }
 }
--- array/src/Matrix.java
+++ array/src/Matrix.java
@@ -5,11 +5,73 @@ public class Matrix {
     this.arr = arr;
   }
 
-  public char[][] getArr() {
-    return arr;
+  boolean isEnd(int i, int j) {
+    return this.arr[i][j] == 'C';
   }
 
-  public void setArr(char[][] arr) {
-    this.arr = arr;
+  boolean isCoordinatesValid(int i, int j) {
+    int maxI = this.arr.length;
+    int maxJ = getMaxJ(i, maxI);
+    return i >= 0 && i < maxI && j >= 0 && j < maxJ;
+  }
+
+  boolean isValidPath(int i, int j) {
+    return (arr[i][j] == 'P' || arr[i][j] == 'S');
+  }
+
+  void markCellAsVisited(int i, int j) {
+    arr[i][j] = 'V';
+  }
+
+  int calculateCrossroads(int i, int j) {
+    int maxI = arr.length;
+    int maxJ = getMaxJ(i, maxI);
+    return getCrossroads(i, j, maxI, maxJ);
+  }
+
+  private int getMaxJ(int i, int maxI) {
+    int maxJ = 0;
+    if (i >= 0 && i < maxI) {
+      maxJ = arr[i].length;
+    }
+    return maxJ;
+  }
+
+  private int getCrossroads(int i, int j, int maxI, int maxJ) {
+    int paths = 0;
+    paths = (ifValidPathBelow(i, j, maxI)) ? paths + 1 : paths;
+    paths = (ifValidPathAbove(i, j)) ? paths + 1 : paths;
+    paths = (ifValidPathLeft(i, j)) ? paths + 1 : paths;
+    paths = (ifValidPathRight(i, j, maxJ)) ? paths + 1 : paths;
+    return paths;
+  }
+
+  private boolean ifValidPathRight(int i, int j, int maxJ) {
+    if (j + 1 < maxJ) {
+      return (arr[i][j + 1] == 'C' || arr[i][j + 1] == 'P');
+    }
+    return false;
+  }
+
+  private boolean ifValidPathLeft(int i, int j) {
+    if (j - 1 >= 0) {
+      return (arr[i][j - 1] == 'C' || arr[i][j - 1] == 'P');
+    }
+    return false;
+  }
+
+  private boolean ifValidPathBelow(int i, int j, int maxI) {
+    if (i + 1 < maxI) {
+      return (arr[i + 1][j] == 'C' || arr[i + 1][j] == 'P');
+    }
+    return false;
+  }
+
+  private boolean ifValidPathAbove(int i, int j) {
+    if (i - 1 >= 0) {
+      return (arr[i - 1][j] == 'C' || arr[i - 1][j] == 'P');
+    }
+    return false;
   }
 }
+
