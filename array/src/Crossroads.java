223b579171de32e7ff7ec2fe53d5c82333c23f59
Witold Zolnowski <blue3656@gmail.com>
2021-08-13 20:57:34 +0100

    working

--- array/src/Crossroads.java
+++ array/src/Crossroads.java
@@ -1,39 +1,81 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Crossroads {
-  int getMax(int i, int j, char[][] arr){
-    return Math.max(Math.max(visit(i+1,j,arr),visit(i-1,j,arr)),Math.max(visit(i,j+1,arr),visit(i,j-1,arr)));
+  int getMax(int i, int j, char[][] arr) {
+    return visit(i, j, arr, 0);
   }
-  int visit(int i, int j, char[][] arr){
-    return calculatePaths(i,j,arr);
+
+  int visit(int i, int j, char[][] arr, int crossroads) {
+    if (isValidCoordinates(i, j, arr)) {
+      if (arr[i][j] == 'C') {
+        return crossroads;
+      }
+      if(arr[i][j] != 'P' && arr[i][j] != 'S'){
+        return crossroads;
+      }
+      arr[i][j] = 'V';
+      //mark visited
+      int p = calculatePaths(i, j, arr);
+      if( p == 0) {
+        return 0;
+      }
+      if (p > 1) {
+        crossroads += p - 1;
+      }
+      return Math.max(
+          Math.max(visit(i + 1, j, arr, crossroads), visit(i - 1, j, arr, crossroads)),
+          Math.max(visit(i, j + 1, arr, crossroads), visit(i, j - 1, arr, crossroads))
+      );
+    } else {
+      return crossroads;
+    }
   }
-  int calculatePaths(int i, int j, char[][] arr){
+
+  private boolean isValidCoordinates(int i, int j, char[][] arr) {
     int maxI = arr.length;
-    int maxJ = arr[i].length;
+    int maxJ = 0;
+    if (i >= 0 && i < maxI) {
+      maxJ = arr[i].length;
+    }
+    if (i >= 0 && i < maxI && j >= 0 && j < maxJ) {
+
+      return true;
+    }
+    return false;
+  }
+
+  int calculatePaths(int i, int j, char[][] arr) {
+    int maxI = arr.length;
+    int maxJ = 0;
+    if (i >= 0 && i < maxI) {
+      maxJ = arr[i].length;
+    }
     int paths = 0;
     //above
-    if(i-1>=0){
-      if(arr[i-1][j] == 'C' || arr[i-1][j] == 'P'){
-        paths +=1;
+    if (i - 1 >= 0) {
+      if (arr[i - 1][j] == 'C' || arr[i - 1][j] == 'P') {
+        paths += 1;
       }
     }
     //below
-    if(i+1<maxI){
-      if(arr[i+1][j] == 'C' || arr[i+1][j] == 'P'){
-        paths +=1;
+    if (i + 1 < maxI) {
+      if (arr[i + 1][j] == 'C' || arr[i + 1][j] == 'P') {
+        paths += 1;
       }
     }
     //left
-    if(j-1>=0){
-      if(arr[i][j-1] == 'C' || arr[i][j-1] == 'P'){
-        paths +=1;
+    if (j - 1 >= 0) {
+      if (arr[i][j - 1] == 'C' || arr[i][j - 1] == 'P') {
+        paths += 1;
       }
     }
     //left
-    if(j+1<maxJ){
-      if(arr[i][j+1] == 'C' || arr[i][j+1] == 'P'){
-        paths +=1;
+    if (j + 1 < maxJ) {
+      if (arr[i][j + 1] == 'C' || arr[i][j + 1] == 'P') {
+        paths += 1;
       }
     }
-    if(paths == 0) return -1;
-    return paths -1;
+    return paths;
   }
 }
--- array/src/CrossroadsTest.java
+++ array/src/CrossroadsTest.java
@@ -15,10 +15,21 @@ class CrossroadsTest {
   }
 
   @Test
+  void returnsZero3() {
+    char[][] arr = {
+        {'S', 'C'},
+        {'P', 'C'}
+    };
+    Crossroads c = new Crossroads();
+    int result = c.getMax(0, 0, arr);
+    assertEquals(1, result);
+  }
+  @Test
   void returnsZero2() {
     char[][] arr = {
         {'S', 'E', 'C'},
-        {'P', 'P', 'P'}
+        {'P', 'P', 'P'},
+        {'E', 'E', 'E'}
     };
     Crossroads c = new Crossroads();
     int result = c.getMax(0, 0, arr);
